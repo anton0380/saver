c977b98bfa3c77ddbbc76f98f85d076e828c9a18
anton0380 <aa@unelma.ru>
2019-10-09 18:56:19 +0300

    Use npm bootstrap and jquery

--- render.js
+++ render.js
@@ -1,5 +1,6 @@
 const screenshot = require('screenshot-desktop');
 const dateFormat = require('dateformat');
+const $ = require('jquery');
 
 let settings = {
     timeout: 5,
@@ -11,5 +12,6 @@ function createScreenshot() {
     let now = new Date();
     let outName = __dirname + '/out/' + dateFormat(now, "yyyy_mm_dd_HH_MM_ss") + '.png';
     screenshot({ filename: outName });
-    document.getElementById('statusalert').textContent = "Save screenshot " + outName;
+    //document.getElementById('statusalert').textContent = "Save screenshot " + outName;
+    $('#statusalert')[0].textContent = "Save screenshot " + outName;
 }
