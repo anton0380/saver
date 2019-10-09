c046b9d982c0453cecbb2173f811e1200d4e3e2e
anton0380 <aa@unelma.ru>
2019-10-09 18:34:03 +0300

    Add function for save screen and correct html

--- main.js
+++ main.js
@@ -6,21 +6,25 @@ let win;
 
 function createWindow() {
     win = new BrowserWindow({
-        width: 700,
-        height: 500,
+        width: 600,
+        height: 400,
         icon: __dirname + '/img/icon.png',
         webPreferences: {
-          nodeIntegration: false
+            nodeIntegration: true
         }
     });
 
-    win.loadFile('index.html')
+    win.setMenu(null);
+
+    win.loadFile('index.html');
 
     //win.webContents.openDevTools();
 
     win.on('closed', () => {
         win = null;
     });
+
+    console.log('window create');
 }
 
 app.on('ready', createWindow);
@@ -28,5 +32,6 @@ app.on('ready', createWindow);
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit();
+        console.log('window close');
     }
 });
--- render.js
+++ render.js
@@ -1,3 +1,15 @@
-function createScreenshot(){
-    document.getElementById('statusalert').textContent = "Need screenshot";
+const screenshot = require('screenshot-desktop');
+const dateFormat = require('dateformat');
+
+let settings = {
+    timeout: 5,
+    use: false,
+    path: __dirname + '/out/'
+}
+
+function createScreenshot() {
+    let now = new Date();
+    let outName = __dirname + '/out/' + dateFormat(now, "yyyy_mm_dd_HH_MM_ss") + '.png';
+    screenshot({ filename: outName });
+    document.getElementById('statusalert').textContent = "Save screenshot " + outName;
 }
